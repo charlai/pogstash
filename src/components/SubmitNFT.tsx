042424fb2e8d600923b42d72f61d4720bf0380f6
Brandon Chow <brandonstchow@gmail.com>
2021-04-17 11:38:48 -0700

    Update usage of mint

--- src/components/SubmitNFT.tsx
+++ src/components/SubmitNFT.tsx
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { TextInput } from './shared/Input';
+import { ZapContractActions } from 'l1-zap-contracts'
+import {
+  hexlify,
+  hexZeroPad,
+} from "ethers/lib/utils"
 
 const Upload = styled.button`
     background-color: #b9a3e3;
@@ -62,13 +67,32 @@ const Submit = styled.button`
     padding: 10px 0px;
 `
 
+const exampleIpfsHash =
+  "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789fedcba"
+const MAX_POSSIBLE_SERIES_TOTAL = 4294967295
+const getRandomSerialNumber = () => Math.floor(Math.random() * MAX_POSSIBLE_SERIES_TOTAL)
+const getRandomSerialNumberBytes4 = () => hexZeroPad(hexlify(getRandomSerialNumber()), 4)
+const maxSeriesTotalBytes4 = hexZeroPad(hexlify(MAX_POSSIBLE_SERIES_TOTAL), 4)
+
 export const SubmitNFT = (props) => {
     const [image, setImage] = useState({ preview: "", raw: "" });
     const [description, setDescription] = useState('');
     const [title, setTitle] = useState('');
+    const [loading, setLoading] = useState(true);
+    // const [walletAccessAllowed, setWalletAccessAllowed] = useState();/ TODO: Should have error if requesting wallet
+    const [zapContract, setZapContract] = useState(null);
+
+    useEffect(async () => {
+        if (zapContract == null) {
+            console.log("requestingWalletAccess!")
+            const walletAccess = await ZapContractActions.requestWalletAccess(window)
+            setZapContract(walletAccess.zapContract)
+            setLoading(false)
+        }
+    }, []);
 
     const hiddenFileInput = useRef(null);
-    
+
     const handleClick = e => {
         hiddenFileInput.current.click();
     };
@@ -88,6 +112,7 @@ export const SubmitNFT = (props) => {
     };
 
     const handleSubmit = e => {
+        ZapContractActions.mintByOwnerForOwner(zapContract, exampleIpfsHash, maxSeriesTotalBytes4, getRandomSerialNumberBytes4())
         e.preventDefault();
     }
 
@@ -96,7 +121,7 @@ export const SubmitNFT = (props) => {
             <NFTUploadContainer>
                 <ImageContainer>
                     {
-                        image.preview ? <img src={image.preview} /> : null  
+                        image.preview ? <img src={image.preview} /> : null
                     }
                 </ImageContainer>
                 <MetaContainer onSubmit={handleSubmit}>
@@ -111,7 +136,7 @@ export const SubmitNFT = (props) => {
                         style={{display: 'none'}}
                         accept="image/png, image/jpeg"
                     />
-                    <TitleInput 
+                    <TitleInput
                         placeholder="Title"
                         type="text"
                         value={description}
